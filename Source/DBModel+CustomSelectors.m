c9b3ff1cd74515032a9a9e913a29331c4aa5d514
Oleg Chebulaev <alegch@bk.ru>
2012-12-16 00:12:35 +0400

    prepare code 3

--- Source/DBModel+CustomSelectors.m
+++ Source/DBModel+CustomSelectors.m
@@ -23,36 +23,42 @@
 + (BOOL)resolveInstanceMethod:(SEL)aSEL
 {
     NSString *attributeName;
+    BOOL isReturn = NO;
     switch((int)[DBModel typeOfSelector:aSEL attributeName:&attributeName]) {
         case DBAttributeSelectorReader: {
                 class_addMethod(self, aSEL, imp_implementationWithBlock(^(DBModel *self){
                     return [self valueForKey:attributeName];
                 }), "@@:");
-            return YES;
+                isReturn = YES;
             }
         break;
         case DBAttributeSelectorWriter: {
                 class_addMethod(self, aSEL, imp_implementationWithBlock(^(DBModel *self, id value) {
                     [self setValue:value forKey:attributeName];
                 }), "v@:@");
-            return YES;
+                isReturn = YES;
             }
         break;
         case DBAttributeSelectorAdder: {
                 class_addMethod(self, aSEL, imp_implementationWithBlock(^(DBModel *self, id value) {
                     [self addRecord:value forKey:attributeName];
                 }), "v@:@");
-            return YES;
+                isReturn = YES;
         }
         break;
         case DBAttributeSelectorRemover: {
                 class_addMethod(self, aSEL, imp_implementationWithBlock(^(DBModel *self, id value) {
                     [self removeRecord:value forKey:attributeName];
                 }), "v@:@");
-            return YES;
+                isReturn = YES;
         }
         break;
     }
+    
+    if (isReturn) {
+        return YES;
+    }
+    
     return [super resolveInstanceMethod:aSEL];
 }
 @end
